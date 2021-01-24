90583b9fe569c71d76cdd85df1b6a886d5abadbe
Abdullah <abdullah.baig@arbisoft.com>
2021-01-24 21:51:00 +0500

    all done

--- teachersPPMG/teachers/admin.py
+++ teachersPPMG/teachers/admin.py
@@ -5,7 +5,8 @@ from .models import TeachersModel
 
 
 class TeachersAdmin(admin.ModelAdmin):
-    list_display = ("email", "first_name", "last_name")
+    list_display = ("email", "first_name", "last_name", "subjects_taught")
+    search_fields = ('email', 'first_name', "subjects_taught")
 
 
 admin.site.register(TeachersModel, TeachersAdmin)
--- teachersPPMG/templates/static/js/merchant.js
+++ teachersPPMG/templates/static/js/merchant.js
@@ -3,23 +3,6 @@ $(document).ready(function () {
     $("#loans").DataTable({
         "order": [[ 0, "desc" ]],
         "pageLength": 50,
-        "columnDefs": [{
-            "targets": 1,
-            "orderable": false
-        },
-        {
-            "targets": 4,
-            "visible": false
-        },
-        {
-            "targets": 5,
-            "visible": false
-        },
-        {
-            "targets": 6,
-            "visible": false
-        }
-        ]
     });
 
     $('#downloads').on('change', function() {
