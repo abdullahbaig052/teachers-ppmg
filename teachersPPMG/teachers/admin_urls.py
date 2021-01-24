d87bd285ee194e2679daf454494afa627271ef80
Abdullah <abdullah.baig@arbisoft.com>
2021-01-24 21:53:53 +0500

    80% done

--- teachersPPMG/teachers/admin_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .admin_views import UploadCSV
-
-urlpatterns = [
-    path('upload-teacher-details/', UploadCSV.as_view(), name="admin_upload_teachers"),
-]
--- teachersPPMG/templates/static/js/installment_amount_toggle.js
+++
@@ -1,8 +0,0 @@
-$(".zmdi-plus").click(function () {
-    $(this).parent().parent().parent().parent().find(".less_details").attr("hidden", true);
-    $(this).parent().parent().parent().parent().find(".more_details").attr("hidden", false);
-});
-$(".zmdi-minus").click(function () {
-    $(this).parent().parent().parent().parent().find(".more_details").attr("hidden", true);
-    $(this).parent().parent().parent().parent().find(".less_details").attr("hidden", false);
-});
