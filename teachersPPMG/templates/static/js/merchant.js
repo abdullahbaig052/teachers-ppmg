69c90b0c8bd8cb78827001c03131d553bee314a6
Abdullah <abdullah.baig@arbisoft.com>
2021-01-24 21:41:46 +0500

    in progress

--- teachersPPMG/templates/static/js/merchant.js
+++ teachersPPMG/templates/static/js/merchant.js
@@ -22,21 +22,6 @@ $(document).ready(function () {
         ]
     });
 
-
-    $("#date_picker").daterangepicker({
-        locale: {
-            format: "YYYY-MM-DD"
-        }
-    });
-
-    $('#date_picker').on('apply.daterangepicker', function(ev, picker) {
-        date_start = picker.startDate.format('YYYY-MM-DD');
-        date_end = picker.endDate.format('YYYY-MM-DD');
-        window.location.href = window.location.pathname + "?date_start=" + date_start + "&date_end=" + date_end;
-    });
-
-
-
     $('#downloads').on('change', function() {
       if (this.selectedIndex !== 0) {
         window.location.href = this.value;
